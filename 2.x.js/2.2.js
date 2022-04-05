a234968d2848bff37e69e4e03dd1578f6128edf4
Eric Chagrikian <echagrikian@hotmail.com>
2022-04-05 16:29:22 +0200

    correct

--- 2.x.js/2.2.js
+++ 2.x.js/2.2.js
@@ -1,10 +1,10 @@
-let min = prompt("select a min equal to 0 and below 9")
-let max = prompt("select a max equal to 0 and below 9")
+let min = Number(prompt("select a min equal to 0 and below 9"))
+let max = Number(prompt("select a max equal to 0 and below 9"))
 
 if ((min > max) || (min < 0) || (max > 9))  {
-function Wtf(){ Error.apply(this, arguments); this.name = "Your min is higher than your max... see you...  ://"; }
-Wtf.prototype = Object.create(Error.prototype);
-throw new Wtf("Damn you're bad, see you...  ://");
+    function Wtf(){ Error.apply(this, arguments); this.name = "Your min is higher than your max, or you chose a number below 0 or above 9... see you...  ://"; }
+    Wtf.prototype = Object.create(Error.prototype);
+    throw new Wtf("Damn you're bad, see you...  ://");
 }
 
 let current = prompt("select a current")

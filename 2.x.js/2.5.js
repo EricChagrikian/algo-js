f2277c24e96f916ce9e3c05b16189f39022ab43b
Eric Chagrikian <echagrikian@hotmail.com>
2022-04-05 15:35:43 +0200

    finish

--- 2.x.js/2.5.js
+++ 2.x.js/2.5.js
@@ -1,9 +1,11 @@
 let num = prompt("Enter your favorite number. Hint: it's the answer to the meaning of life")
 
 if (num == "42") {
-        alert("Great job!")
+        alert("Great job! That is everyone's favorite number, right... ? :)")
 } else { while (num != "42") {
-        alert("Nope! please try again.")
+        alert("Nope! please try again.");
+        prompt("That wasn't your favorite number! Think about life.");
+        if (num == "42");
         break;
     }
 }

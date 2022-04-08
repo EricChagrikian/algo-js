e1261e047adcb720261e38c60b9e20d67a5092ca
Eric Chagrikian <echagrikian@hotmail.com>
2022-04-08 11:54:32 +0200

    fin

---
+++ 3.x.js/3.1.js
@@ -0,0 +1,13 @@
+var arr = [1, 2, 3, 4, 5];
+var total = 0;
+for (var i in arr) {
+  total += arr[i];
+}
+
+// 2e arr
+
+var arr = [100, 101, 102];
+var total = 0;
+for (var i in arr) {
+  total += arr[i];
+}

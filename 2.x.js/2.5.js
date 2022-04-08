da77d5161535e23a4c2080995ad9e3054028a6dd
Eric Chagrikian <echagrikian@hotmail.com>
2022-04-08 11:24:24 +0200

    correct error

--- 2.x.js/2.5.js
+++ 2.x.js/2.5.js
@@ -1,11 +1,11 @@
 let num = prompt("Enter your favorite number. Hint: it's the answer to the meaning of life")
-
+ 
 if (num == "42") {
         alert("Great job! That is everyone's favorite number, right... ? :)")
-} else { while (num != "42") {
+} else if (num != "42") { while (num != "42") {
         alert("Nope! please try again.");
-        prompt("That wasn't your favorite number! Think about life.");
-        if (num == "42");
-        break;
-    }
+        num = prompt("That wasn't your favorite number! Think about life.");
+        }
+        if (num == "42")
+        {alert("Great job! That is everyone's favorite number, right... ? :)")}
 }

95233755c98138f6a100dbc92fa5ff9a8ca628eb
Eric Chagrikian <echagrikian@hotmail.com>
2022-04-08 14:26:18 +0200

    end

--- 2.x.js/2.7.js
+++ 2.x.js/2.7.js
@@ -1,6 +1,11 @@
 let n = Number(prompt("Enter how many numbers you want to sum"));
+let final = 0;
 
 while (n !== 0){
-    Number(prompt("Enter a number to sum"))
+    ask = Number(prompt("Enter a number to sum"));
+    final += ask;
     n--
 }
+
+console.log ("The result is " + final)
+

b53e0d9e2ca65eb467b6f25cc0c0ef0f3b9f9336
Eric Chagrikian <echagrikian@hotmail.com>
2022-04-08 12:25:08 +0200

    let instead of var

--- 3.x.js/3.1.js
+++ 3.x.js/3.1.js
@@ -1,13 +1,13 @@
-var arr = [1, 2, 3, 4, 5];
-var total = 0;
-for (var i in arr) {
+let arr = [1, 2, 3, 4, 5];
+let total = 0;
+for (let i in arr) {
   total += arr[i];
 }
 
 // 2e arr
 
-var arr = [100, 101, 102];
-var total = 0;
-for (var i in arr) {
+let arr = [100, 101, 102];
+let total = 0;
+for (let i in arr) {
   total += arr[i];
 }

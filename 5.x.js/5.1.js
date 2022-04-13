968bde3617bf10dc1ad3334ef8c1e6b9dd2b3584
Eric Chagrikian <echagrikian@hotmail.com>
2022-04-13 15:57:21 +0200

    5.1 done

---
+++ 5.x.js/5.1.js
@@ -0,0 +1,14 @@
+
+let askTvSerie = {
+   
+    name: prompt("What is your favorite TV serie's name ?"),
+    year: prompt("When was it produced ?"),
+    cast: prompt ("Name one or more members of its cast.")
+
+};
+
+
+
+        console.log("Your favorite serie's name is: " + askTvSerie["name"])
+        console.log("It was produced in: " + askTvSerie["year"])
+        console.log("One of/Some of the member(s) of its cast: " + askTvSerie["cast"])
